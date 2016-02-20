aca25ab73a69e8dcde8f401a87aa85ea33abe883
Kapil Yedidi <kapil.yedidi@gmail.com>
2016-02-20 13:13:16 -0800

    Dynamic number of columns is working. Now trying to center the columns.

--- v2/site2.js
+++ v2/site2.js
@@ -1,5 +1,82 @@
 var listView = null;
 // var compiled = _.template('<span><img class="lazy-img" data-original="${image_url}" height="400" width="400" /></span><br/>');
+var columns = null;
+var nextIndexForPhoto = 0;
+var raw_data = null;
+
+var imageWidth = null;
+var columnBorderWidth = null;  // border on each side of a column
+
+function row() {
+    var colIndex, length, $minCol, $currCol;
+    for (var i = 0, length = columns.length; i < length; i++) {// for(index = 0, length = columns.length; index < length; index++) {
+
+
+        // Determine the min column (column with the minimum length)
+        // We do this so each column is the same length
+        var $minCol = null;
+        for(colIndex = 0; colIndex < length; colIndex++) {
+            $currCol = $(columns[colIndex]);
+            if(!$minCol) $minCol = $currCol;
+
+            // The bug is that the CSS in the HTML makes each column think it's the same height
+            if ($currCol.height() <= $minCol.height()) {
+                $minCol = $currCol;
+            }
+            //  else $minCol = $minCol.height() > $currCol.height() ? $currCol : $minCol;
+        }
+        /*
+        for(colIndex = 0; colIndex < length; colIndex++) {
+            $currCol = $(columns[colIndex]);
+
+            if(!$minCol) $minCol = $currCol;
+            else $minCol = $minCol.height() > $currCol.height() ? $currCol : $minCol;
+        }
+        */
+
+        // If we don't have any more results to show
+        if (nextIndexForPhoto > raw_data.length) {
+            // TODO - I need to handle this case better so it doesn't get here.
+            // Best I can do for now.
+            return;
+        }
+
+        // console.log(JSON.stringify(raw_data[i]));
+        var current = raw_data[nextIndexForPhoto];
+
+        var image_id = current.photos[0];  // we take the first image
+        var image_url = 'http://imgur.com/' + image_id;
+
+        // image_url = image_url.substr(0, image_url.length-4);
+        var image_url_large = image_url + "l.jpg";
+        var image_url_medium = image_url + "m.jpg";
+        var image_url_small = image_url + "s.jpg";
+
+        // TODO - use something cleaner like strcat
+        var gender_string = "";
+        if (current.gender == true) {
+            gender_string = 'female';
+        } else {
+            gender_string = 'male';
+        }
+
+
+
+
+        // all_grid_element_html.push(grid_elemen);
+        // all_grid_element_html = all_grid_element_html + grid_element_html;
+        // var html = compiled({'image_url': image_url_medium});
+        var html = '<div><img class="lazy-img" data-original="' + image_url_medium + '" height="400" width="400" /></div>';
+
+        // Append it to the column with the lowest height
+        $minCol.data('listView').append(html);
+        nextIndexForPhoto++;
+        // listView.append(html);
+        // console.log('appending: ' + c);
+        // alert(result[i]);
+        //Do something
+    }
+}
 
 function downloadContent(){
     Papa.parse("csv_dump.csv", {
@@ -9,7 +86,8 @@ function downloadContent(){
         skipEmptyLines:true,
         complete: function(results) {
 
-            var LIMIT = 100000;  // Just for testing
+            var LIMIT = 100;  // Just for testing
+
 
             // Using JSON
             // raw_data = data.result;
@@ -24,43 +102,15 @@ function downloadContent(){
                 raw_data[i]['photos'] = raw_data[i].photos.split(',');
             }
 
+
             //
             var all_grid_element_html = [];
-            for (var i = 0; i < raw_data.length; i++) {
-                if (i > LIMIT) {
-                    break;
-                }
-                // console.log(JSON.stringify(raw_data[i]));
-                var current = raw_data[i];
-
-                var image_id = current.photos[0];  // we take the first image
-                var image_url = 'http://imgur.com/' + image_id
-
-                // image_url = image_url.substr(0, image_url.length-4);
-                var image_url_large = image_url + "l.jpg"
-                var image_url_medium = image_url + "m.jpg"
-                var image_url_small = image_url + "s.jpg"
-
-                // TODO - use something cleaner like strcat
-                var gender_string = "";
-                if (current.gender == true) {
-                    gender_string = 'female';
-                } else {
-                    gender_string = 'male';
-                }
 
 
-
-
-                // all_grid_element_html.push(grid_elemen);
-                // all_grid_element_html = all_grid_element_html + grid_element_html;
-                // var html = compiled({'image_url': image_url_medium});
-                var html = '<div><img class="lazy-img" data-original="' + image_url_medium + '" height="400" width="400" /></div>';
-                listView.append(html);
-                // console.log('appending: ' + c);
-                // alert(result[i]);
-                //Do something
+            for (var i = 0; i < LIMIT; i ++ ) {
+                row();
             }
+
             // Now that everything is added, NOW call the layout method
             // layoutGrid()
 
@@ -84,18 +134,55 @@ function getMethods(obj) {
 
 $(document).ready(function() {
     console.log('hello!');
-    var $el = $('#my-infinite-container');
-    listView = new infinity.ListView($el, {
-        lazy: function(){
-            $(this).find('.lazy-img').each(function() {
-                var $ref = $(this);
-                $ref.attr('src', $ref.attr('data-original'));
-            });
-            // console.log('elem data: ' + getMethods(elem));
-            // console.log('elem attr: ' + elem.getAttribute('data-original'));
-            // $(elem).attr('src', $(elem).attr('data-original'));}
-        }
+    // var $el = $('#my-infinite-container');
+
+
+    // TODO - assert
+    imageWidth = $('.left-floater').css('width');
+    columnBorderWidth = $('.left-floater').css('border-left-width');
+    console.log('imageWidth:' + imageWidth);
+    console.log('columnBorderWidth:' + columnBorderWidth);
+    console.log('windowWidth: ' + $( window ).width());
+    console.log('documentWidth: ' + $( document ).width());
+
+    var num_columns = 2;
+    for (var i = 0; i < num_columns; i++) {
+        var html = '<div class="infinite infinite-scroll-column"></div>';
+        $('#float-wrap').append(html);
+    }
+
+
+    columns = $('.infinite');
+    console.assert(columns.length > 0, "No columns present!");
+
+    columns.each(function() {
+        listView = new infinity.ListView($(this), {
+            lazy: function(){
+                $(this).find('.lazy-img').each(function() {
+                    var $ref = $(this);
+                    $ref.attr('src', $ref.attr('data-original'));
+                });
+                // console.log('elem data: ' + getMethods(elem));
+                // console.log('elem attr: ' + elem.getAttribute('data-original'));
+                // $(elem).attr('src', $(elem).attr('data-original'));}
+            }
+        });
+        $(this).data('listView', listView);
     });
+
+    /*
+    columns.each(function() {
+        var listView = new ListView($(this), {
+            lazy: function() {
+                $(this).find('.pug').each(function() {
+                    var $ref = $(this);
+                    $ref.attr('src', $ref.attr('data-original'));
+                });
+            }
+        });
+        $(this).data('listView', listView);
+    });
+    */
 
     // Lazy load images
     downloadContent();
